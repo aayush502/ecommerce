382513c60c03c91a0a6d759783eca16821f2339f
Aayush502 <humagain.aayush83@gmail.com>
2021-11-01 21:21:22 +0545

    dsafga

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -82,10 +82,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'ecommerce',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
